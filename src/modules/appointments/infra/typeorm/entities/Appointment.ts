1eae82828656648a2ed1cf843ee677cbcdb2fa16
Tiago Pereira <tiagohcp@gmail.com>
2020-07-13 17:36:26 -0300

    Implementando DDD

--- src/modules/appointments/infra/typeorm/entities/Appointment.ts
+++ src/modules/appointments/infra/typeorm/entities/Appointment.ts
@@ -1,4 +1,3 @@
-/* eslint-disable camelcase */
 import {
   Entity,
   Column,
@@ -9,7 +8,7 @@ import {
   JoinColumn,
 } from 'typeorm';
 
-import User from './User';
+import User from '@modules/users/infra/typeorm/entities/User';
 
 @Entity('appointments')
 class Appointment {
